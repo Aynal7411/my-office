94cfbb89f9c51bb249f95c5e2c036d5a57d6fff2
Aynal-dev <aynaldevgen@gmail.com>
2026-05-23 11:14:34 +0600

    some fixing issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,5 @@
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
-import Home from './pages/Home';
-import Admin from './pages/Admin';
 import Navbar from './components/Navbar';
 
 function App() {
@@ -10,10 +8,7 @@ function App() {
     <div className="min-h-screen bg-slate-50 text-slate-900 dark:bg-slate-950 dark:text-slate-100 transition-colors duration-300">
       <Navbar />
       <AnimatePresence mode="wait" initial={false}>
-        <Routes location={location} key={location.pathname}>
-          <Route path="/" element={<Home />} />
-          <Route path="/admin" element={<Admin />} />
-        </Routes>
+        <Outlet key={location.pathname} />
       </AnimatePresence>
     </div>
   );
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
+import Home from './pages/Home';
+import Admin from './pages/Admin';
 import './index.css';
 import { ThemeProvider } from './context/ThemeContext';
+
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: 'admin', element: <Admin /> },
+      ],
+    },
+  ],
+  { future: { v7_relativeSplatPath: true } }
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
       <ThemeProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+        <RouterProvider router={router} />
       </ThemeProvider>
     </HelmetProvider>
   </React.StrictMode>
